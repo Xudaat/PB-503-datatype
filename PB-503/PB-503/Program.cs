425e102d157db5091ef604db01e039e1852188ae
User <xudaatm@gmail.com>
2024-10-11 21:29:53 +0400

    data type

---
+++ PB-503/PB-503/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel.Design;
+using System.Threading.Tasks.Sources;
+
+//namespace PB_503
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        { 
+            //#region task2
+            //{
+            //    console.writeline("pleas enter your score:80");
+            //}
+            //var score = 80;
+            //{
+            //    console.writeline(score);
+            //}
+            //if (score < 65)
+
+            //{
+            //    console.writeline("kesildin");
+            //}
+            //else if (score >= 65 && score < 85)
+
+            //{
+            //    console.writeline("adi diplom");
+            //}
+            //else if (score >= 85 && score < 95)
+
+            //{
+            //    console.writeline("şeref diplomu");
+            //}
+            //else if (score >= 95)
+            //{
+            //    console.writeline("yuksek şeref diplomu");
+            //}
+            //#endregion
+    //        #region Task1
+
+    //        for (int i = 1; i <= 100; i++)
+    //        {
+
+    //            if (i % 3 == 0 && i % 5 == 0)
+    //            {
+    //                Console.WriteLine(i);
+    //            }
+    //        }
+    //    }
+    //}
+    //       #endregion
+
+    //        }
+        
+                
